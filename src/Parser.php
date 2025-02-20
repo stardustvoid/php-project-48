841b60e5245609e7c56899fbb6f9f53cc4bb1b32
stardustvoid <112036089+stardustvoid@users.noreply.github.com>
2025-02-20 15:39:23 +0700

    Add yaml parsing

--- src/Parser.php
+++ src/Parser.php
@@ -2,15 +2,28 @@
 
 namespace Differ\Parser;
 
+use Symfony\Component\Yaml\Yaml;
+
 function getFileFullPath(string $path)
 {
     return realpath($path);
 }
 
-function parse(string $filePath)
+function convertToPhp(string $fileContent, string $extension): object
+{
+    $phpObject = match ($extension) {
+        'json' => json_decode($fileContent),
+        'yaml', 'yml' => Yaml::parse($fileContent, Yaml::PARSE_OBJECT_FOR_MAP)
+    };
+
+    return $phpObject;
+}
+
+function parse(string $filePath): object
 {
     $fileFullPath = getFileFullPath($filePath);
+    $extension = pathinfo($fileFullPath, PATHINFO_EXTENSION);
     $content = file_get_contents($fileFullPath);
 
-    return json_decode($content);
+    return convertToPhp($content, $extension);
 }
