278d9c2301c3d778244f101e958c951c77fc3274
stardustvoid <112036089+stardustvoid@users.noreply.github.com>
2025-02-20 15:17:55 +0700

    Add tests for flat yaml

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -8,20 +8,37 @@ use function Differ\Differ\genDiff;
 
 class DifferTest extends TestCase
 {
+    private $flatResult;
+    // private $result;
+
     public function getFixtureFullPath($fixtureName)
     {
         $parts = [__DIR__, 'fixtures', $fixtureName];
         return realpath(implode('/', $parts));
     }
 
+    public function setUp(): void
+    {
+        $this->flatResult = $this->getFixtureFullPath('flat_result.txt');
+    }
+
     public function testFlatJson()
     {
         $json1 = $this->getFixtureFullPath('file1.json');
         $json2 = $this->getFixtureFullPath('file2.json');
-        $result = $this->getFixtureFullPath('flat_result.txt');
 
         $diff = genDiff($json1, $json2);
 
-        $this->assertStringEqualsFile($result, $diff);
+        $this->assertStringEqualsFile($this->flatResult, $diff);
+    }
+
+    public function testFlatYaml()
+    {
+        $yaml1 = $this->getFixtureFullPath('file1.yaml');
+        $yaml2 = $this->getFixtureFullPath('file2.yaml');
+
+        $diff = genDiff($yaml1, $yaml2);
+
+        $this->assertStringEqualsFile($this->flatResult, $diff);
     }
 }
